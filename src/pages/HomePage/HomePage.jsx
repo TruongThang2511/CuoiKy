303c660d77770a61df1881b9112b7738bba4194d
Lư Trường Thắng <lutruongthang2511@gmail.com>
2024-04-18 13:09:48 +0700

    Initial commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import TypeProduct from '../../components/TypeProduct/TypeProduct'
+import { WrapperButtonMore, WrapperProducts, WrapperTypeProduct } from './style'
+import CardComponent from '../../components/CardComponent/CardComponent'
+import { useQuery } from '@tanstack/react-query'
+import * as ProductService from '../../services/ProductService'
+
+const HomePage = () => {
+  const arr = ['TV', 'Tu lanh', 'Lap top']
+  const fetchProductAll = async () => {
+    const res = await ProductService.getAllProduct()
+    console.log('res', res)
+    return res
+  }
+  const {isLoading, data: products} = useQuery(['products'], fetchProductAll, { retry: 3, retryDelay: 1000 })
+  console.log('data', products)
+  return (
+    <>
+      <div style={{ width: '1270px', margin: '0 auto' }}>
+        <WrapperTypeProduct>
+          {arr.map((item) => {
+            return (
+              <TypeProduct name={item} key={item} />
+            )
+          })}
+        </WrapperTypeProduct>
+      </div>
+      <div className='body' style={{ width: '100%', backgroundColor: '#efefef', }}>
+        <div id="container" style={{ height: '1000px', width: '1270px', margin: '0 auto' }}>
+          <WrapperProducts>
+            {products?.data?.map((product) => {
+              return (
+                <CardComponent 
+                  key={product._id} 
+                  countInStock={product.countInStock} 
+                  description={product.description} 
+                  image={product.image} 
+                  name={product.name} 
+                  price={product.price}
+                  rating={product.rating}
+                  type={product.type}
+                  selled={product.selled}
+                  discount={product.discount}
+                />
+              )
+            })}
+          </WrapperProducts>
+          <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+            <WrapperButtonMore textButton="Xem thêm" type="outline" styleButton={{
+              border: '1px solid rgb(11, 116, 229)', color: 'rgb(11, 116, 229)',
+              width: '240px', height: '38px', borderRadius: '4px'
+            }}
+              styleTextButton={{ fontWeight: 500 }} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default HomePage 
